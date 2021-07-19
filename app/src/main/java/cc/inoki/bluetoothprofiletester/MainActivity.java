ff67d9071f957cffc120f98b2ea4d3c761c58842
Inoki <veyx.shaw@gmail.com>
2021-07-19 23:34:58 +0200

    Add impl for mouse left button simple click

--- app/src/main/java/cc/inoki/bluetoothprofiletester/MainActivity.java
+++ app/src/main/java/cc/inoki/bluetoothprofiletester/MainActivity.java
@@ -13,29 +13,36 @@ import android.bluetooth.BluetoothProfile;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 
  public class MainActivity extends AppCompatActivity {
 
-    class BluetoothProfileController extends BluetoothHidDevice.Callback implements BluetoothProfile.ServiceListener {
+     class BluetoothProfileController extends BluetoothHidDevice.Callback implements BluetoothProfile.ServiceListener {
         private static final String TAG = "BluetoothProfileController";
 
         /* HID service */
         private BluetoothHidDeviceAppSdpSettings sdpSettings =
                 new BluetoothHidDeviceAppSdpSettings("InokiHID", "Inoki's HID profile tester", "Inoki",
-                        BluetoothHidDevice.SUBCLASS1_COMBO, new byte[] {0x05, 0x01, 0x09, 0x02});
+                        BluetoothHidDevice.SUBCLASS1_COMBO,
+                        Descriptors.MOUSE_KEYBOARD_COMBO);
 
         private BluetoothHidDeviceAppQosSettings qosSettings = new BluetoothHidDeviceAppQosSettings(
                 BluetoothHidDeviceAppQosSettings.SERVICE_BEST_EFFORT, 800, 9, 0, 11250, BluetoothHidDeviceAppQosSettings.MAX);
+
+        private BluetoothHidDevice hidDevice;
+
         @Override
         public void onServiceConnected(int profile, BluetoothProfile bluetoothProfile) {
             Log.d(TAG, "Service connected!");
             if (profile == BluetoothProfile.HID_DEVICE) {
                 if (bluetoothProfile instanceof BluetoothHidDevice) {
-                    // TODO: store it in class
-                    BluetoothHidDevice hidDevice = (BluetoothHidDevice) bluetoothProfile;
+                    // Store it in class
+                    hidDevice = (BluetoothHidDevice) bluetoothProfile;
                     hidDevice.registerApp(sdpSettings, null, qosSettings, new Executor() {
                         @Override
                         public void execute(Runnable runnable) {
@@ -52,6 +59,8 @@ import java.util.concurrent.ExecutorService;
         @Override
         public void onServiceDisconnected(int profile) {
             Log.d(TAG, "Disconnected to service");
+            if (profile == BluetoothProfile.HID_DEVICE)
+                hidDevice = null;
         }
 
         /* Constructor */
@@ -61,58 +70,137 @@ import java.util.concurrent.ExecutorService;
             BluetoothAdapter.getDefaultAdapter().getProfileProxy(MainActivity.this, this, BluetoothProfile.HID_DEVICE);
         }
 
+        private BluetoothDevice hostDevice = null;
+
+        public class MouseReportSender {
+            public MouseReport mouseReport = new MouseReport();
+
+            protected void sendMouse() {
+                if (!hidDevice.sendReport(hostDevice, MouseReport.ID, mouseReport.getReport())) {
+                    Log.e(TAG, "Report wasn't sent");
+                } else {
+                    Log.i(TAG, (int)mouseReport.getReport()[0] + " sent");
+                }
+            }
+
+            public void sendLeftClickOn() {
+                mouseReport.setLeftButton(true);
+                sendMouse();
+            }
+            public void sendLeftClickOff() {
+                mouseReport.setLeftButton(false);
+                sendMouse();
+            }
+        }
+
         /* Callback events */
         public void onAppStatusChanged(BluetoothDevice pluggedDevice, boolean registered) {
             super.onAppStatusChanged(pluggedDevice, registered);
             Log.i(TAG, "onAppStatusChanged");
+
+            if (registered) {
+                List<BluetoothDevice> pairedDevices = hidDevice.getDevicesMatchingConnectionStates(new int[] {
+                        BluetoothProfile.STATE_CONNECTING,
+                        BluetoothProfile.STATE_CONNECTED,
+                        BluetoothProfile.STATE_DISCONNECTED,
+                        BluetoothProfile.STATE_DISCONNECTING
+                });
+                if (pairedDevices.size() > 0) {
+                    Log.d("paired d", "paired devices found");
+                    BluetoothDevice firstPairedDevice = pairedDevices.get(0);
+
+                    if (hidDevice.getConnectionState(pluggedDevice) == BluetoothProfile.STATE_DISCONNECTED && pluggedDevice != null) {
+                        hidDevice.connect(pluggedDevice);
+                        mouseSender = new MouseReportSender();
+                    } else if (hidDevice.getConnectionState(firstPairedDevice) == BluetoothProfile.STATE_DISCONNECTED) {
+                        hidDevice.connect(firstPairedDevice);
+                        mouseSender = new MouseReportSender();
+                    }
+                } else {
+                    Log.i("error", "No paired device");
+                }
+            } else {
+                Log.i(TAG, "Not registered");
+            }
         }
 
         public void onConnectionStateChanged(BluetoothDevice device, int state) {
             Log.i(TAG, "onConnectionStateChanged");
+            if (state == BluetoothProfile.STATE_CONNECTED) {
+                if (device != null) {
+                    hostDevice = device;
+                    Log.e(TAG, "Device connected");
+                    // TODO: Callback for connection
+                } else {
+                    Log.e(TAG, "Device not connected");
+                }
+            } else {
+                hostDevice = null;
+                if(state == BluetoothProfile.STATE_DISCONNECTED)
+                {
+                    // TODO: Callback for disconnection
+                }
+            }
         }
 
         public void onGetReport(BluetoothDevice device, byte type, byte id, int bufferSize) {
             super.onGetReport(device, type, id, bufferSize);
             Log.i(TAG, "onGetReport");
+
+            if (type == BluetoothHidDevice.REPORT_TYPE_FEATURE) {
+                Log.i(TAG, "Feature report received");
+                /*
+                featureReport.wheelResolutionMultiplier = true
+                featureReport.acPanResolutionMultiplier = true
+                Log.i("getbthid","$btHid")
+
+                var wasrs=btHid?.replyReport(device, type, FeatureReport.ID, featureReport.bytes)
+                Log.i("replysuccess flag ",wasrs.toString())
+                */
+            }
         }
 
         public void onSetReport(BluetoothDevice device, byte type, byte id, byte[] data) {
             super.onSetReport(device, type, id, data);
             Log.i(TAG, "onSetReport");
         }
+     }
 
-//        public void onSetProtocol(BluetoothDevice device, byte protocol) {
-//            super.onSetReport(device, type, id, data);
-//            Log.i(TAG, "onSetReport");
-//        }
+     private BluetoothProfileController controller;
+     private BluetoothProfileController.MouseReportSender mouseSender;
+     @Override
+     protected void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+         setContentView(R.layout.activity_main);
 
-//        public void onInterruptData(BluetoothDevice device, byte reportId, byte[] data) {
-//            throw new RuntimeException("Stub!");
-//        }
+         if (controller == null) {
+             if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                 controller = new BluetoothProfileController();
 
-//        public void onVirtualCableUnplug(BluetoothDevice device) {
-//            throw new RuntimeException("Stub!");
-//        }
-
-    }
-
-    private BluetoothProfileController controller;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        if (controller == null) {
-            if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                controller = new BluetoothProfileController();
-            } else {
-                Log.i("Main", "Cannot create controller due to restricted permission");
-                requestPermissions(
-                        new String[] { Manifest.permission.ACCESS_COARSE_LOCATION }, 0
-                );
-            }
-        }
-    }
+                 ((Button)findViewById(R.id.left_button)).setOnClickListener(new View.OnClickListener() {
+                     @Override
+                     public void onClick(View v) {
+                         if (mouseSender != null) {
+                             mouseSender.sendLeftClickOn();
+                             try {
+                                 Thread.sleep(10);
+                             } catch (InterruptedException e) {
+                                 e.printStackTrace();
+                             }
+                             mouseSender.sendLeftClickOff();
+                         } else {
+                             Log.i("Test", "No device connected");
+                         }
+                     }
+                 });
+             } else {
+                 Log.i("Main", "Cannot create controller due to restricted permission");
+                 requestPermissions(
+                         new String[] { Manifest.permission.ACCESS_COARSE_LOCATION }, 0
+                 );
+             }
+         }
+     }
 
      @Override
      protected void onStop() {
